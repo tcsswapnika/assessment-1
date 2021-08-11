a57e290e789a5c86aa1918c1dc3aa0859500e306
tcsswapnika <tcsswapnika@gmail.com>
2021-08-11 13:17:12 +0000

    code updated

--- src/com/tcs/bankAssessment/SavingsAccount.java
+++ src/com/tcs/bankAssessment/SavingsAccount.java
@@ -2,13 +2,12 @@ package com.tcs.bankAssessment;
 
 import java.util.Date;
 
-public class SavingsAccount extends BankAccount{ 
+public class SavingsAccount extends BankAccount {
 
-	public SavingsAccount(int accountNumber, Double balance, String ownerNamer,Date createDate,String type,
-				String status){
-			super(accountNumber, balance, createDate, type, status);
-		
-		}
-	
+	public SavingsAccount(int accountNumber, Double balance, String ownerNamer, Date createDate, String type,
+			String status) {
+		super(accountNumber, balance, createDate, type, status);
+
+	}
+
 }
-
